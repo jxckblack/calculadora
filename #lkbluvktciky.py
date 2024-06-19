185c024c7ac78c3b628c69814ceaf9dc756b8cbf
MigzaRaad <mi.raad@duocuc.cl>
2024-06-19 15:25:45 -0400

    Update #lkbluvktciky.py

--- #lkbluvktciky.py
+++ #lkbluvktciky.py
@@ -1,4 +1,15 @@
 #tenemos que crear una calculadora para fundamentos 
 
 #Resta
-def
+
+
+#suma
+def suma(a,b):
+     suma= a+b
+     return suma
+
+
+#multiplicación
+def multiplicación (a,b):
+      multiplicación=a*b
+      return multiplicación
